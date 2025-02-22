5a98d65dcdc309e62ecf05db22086c7043371822
Aayush501 <shrivastavashu501@gmail.com>
2025-02-22 13:34:38 +0530

    [Aayush Kumar Shrivastav] Add . UC4 Post Request -> Solution

--- src/main/java/com/apicalls/requests/controllers/GetHello.java
+++ src/main/java/com/apicalls/requests/controllers/GetHello.java
@@ -2,13 +2,35 @@ package com.apicalls.requests.controllers;
 
 import org.springframework.web.bind.annotation.*;
 
+// created class HelloName
+class HelloName {
+    // attributes of this class
+    String firstName;
+    String lastName;
+
+    HelloName() {
+        firstName = "Aayush";
+        lastName = "Kumar";
+    }
+
+    // getters method
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
+
 @RestController
 public class GetHello {
 
-    // sayHello method that gets data from /:port/hello/{name}
-    @GetMapping("/hello/{name}")
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name + " From BridgeLabz!!";
+    // sayHello method that gets data from /:port/hello
+    @PostMapping("/hello")
+    public String sayHello(@RequestBody HelloName hello) {
+        return "Hello " + hello.getFirstName() + " " + hello.getLastName() + " From BridgeLabz!!";
     }
 
     //route to test this => http://localhost:8080/hello/Aayush
