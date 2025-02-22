644d456c6aa1870cec0552bd3f4b0f57ab652d22
Aayush501 <shrivastavashu501@gmail.com>
2025-02-22 11:13:08 +0530

    [Aayush Kumar Shrivastav] Fix . GetHello -> added GetMapping annotation

--- src/main/java/com/apicalls/requests/controllers/GetHello.java
+++ src/main/java/com/apicalls/requests/controllers/GetHello.java
@@ -1,13 +1,12 @@
 package com.apicalls.requests.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetHello {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String sayHello() {
         return "Hello from BridgeLabz!!!";
     }
